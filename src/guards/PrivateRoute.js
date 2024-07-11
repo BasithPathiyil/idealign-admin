79e67caea4ad225cd4ef42a36fbd7d1b6630ec3a
BasithPathiyil <basithpathiyil007@gmail.com>
2024-07-11 23:51:47 +0530

    changes

--- src/guards/PrivateRoute.js
+++ src/guards/PrivateRoute.js
@@ -1,23 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { Navigate, useLocation } from 'react-router-dom';
-import { useEffect } from 'react';
 import { handleToken } from 'utils/jwt';
 import { LOGIN_SUCCESS } from 'store/auth/constants';
-import { useState } from 'react';
 
 const PrivateRoute = ({ children }) => {
-  const isTokenPresent = () => !!localStorage.getItem('accessToken');
-  if (isTokenPresent()) {
-    let token = localStorage.getItem('accessToken');
-    handleToken(token);
-  }
-  console.log('!isTokenPresent()', !isTokenPresent());
-  if (!isTokenPresent()) {
+  const dispatch = useDispatch();
+  const [isVerified, setIsVerified] = useState(null);
+  const location = useLocation();
 
-    return <Navigate to="/login" />;
+  const verifyToken = async (token) => {
+    try {
+      console.log('working evry');
+      const response = await fetch('http://localhost:5000/api/verify_token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
+        }
+      });
+
+      if (!response.ok) {
+        throw new Error('Token verification failed');
+      }
+
+      const data = await response.json();
+      // Assuming the response contains user data
+      dispatch({ type: LOGIN_SUCCESS, payload: data });
+      handleToken(token);
+      setIsVerified(true);
+    } catch (error) {
+      console.error(error);
+      handleToken(null);
+      setIsVerified(false);
+    }
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      verifyToken(token);
+    } else {
+      setIsVerified(false);
+    }
+  }, [dispatch]);
+
+  if (isVerified === null) {
+    // You can return a loading spinner or null while the token is being verified
+    return <div>Loading...</div>;
   }
+
+  if (!isVerified) {
+    return <Navigate to="/login" state={{ from: location }} />;
+  }
+
   return <>{children}</>;
 };
 
